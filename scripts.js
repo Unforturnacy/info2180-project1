e6770f4e7691368c14960fac95ad4886810c2bd1
Teric Simons <tericsimons12@gmail.com>
2022-10-30 23:03:49 -0500

    updated javascript file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function(){
+    main();
+})
+
+function main()
+{
+    var btn = document.getElementsByTagName("button")[0];
+    var email = document.getElementById("email");
+    var mes = document.getElementsByClassName("message")[0];
+    btn.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (email.value === "" || email.value === null)
+        {
+            mes.innerHTML = "Please enter a valid email address.";
+        }
+        else
+        {
+            mes.innerHTML = "Thank you! Your email address, " + email.value + ", has been added to our mailing list.";
+        }
+ });
+}
